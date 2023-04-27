varigit/vendor-variscite-am62x

commit 52265062f8ee871e9553ba3189a57755a5e4aa55

am62x: firmware: add ti-sysfw stub bin
Add ti-sysfw stub firmware to support suspend/resume.

Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

am62x.mk
@@ -46,3 +46,6 @@ PRODUCT_PACKAGES += \
 	wl18xx-fw-4.bin \
 	wl18xx-conf.bin
 
+# ti-sysfw
+PRODUCT_PACKAGES += \
+	ti-fs-stub-firmware-am62x-gp-signed

firmware/Android.bp
@@ -1,0 +1,8 @@
+// ti-sysfw firmware
+prebuilt_firmware {
+    name: "ti-fs-stub-firmware-am62x-gp-signed",
+    vendor: true,
+    src: "vendor/firmware/ti-sysfw/ti-fs-stub-firmware-am62x-gp-signed.bin",
+    sub_dir: "ti-sysfw",
+    proprietary: true,
+}

commit 673d170643fcccc9c4fe5449f0661f22918da3b3

am62x: firmware: fix ti-sysfw stub name
Actually file was copying in vendor/firmware/ti-sysfw/ with name
ti-fs-stub-firmware-am62x-gp-signed instead of
ti-fs-stub-firmware-am62x-gp-signed.bin

So on boot when we go in suspend ti-sci driver not found firmware.

Fix it to have good filename.

Fixes: 52265062f8ee ("am62x: firmware: add ti-sysfw stub bin")
Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

am62x.mk
@@ -48,4 +48,4 @@ PRODUCT_PACKAGES += \
 
 # ti-sysfw
 PRODUCT_PACKAGES += \
-	ti-fs-stub-firmware-am62x-gp-signed
+	ti-fs-stub-firmware-am62x-gp-signed.bin

firmware/Android.bp
@@ -1,6 +1,6 @@
 // ti-sysfw firmware
 prebuilt_firmware {
-    name: "ti-fs-stub-firmware-am62x-gp-signed",
+    name: "ti-fs-stub-firmware-am62x-gp-signed.bin",
     vendor: true,
     src: "vendor/firmware/ti-sysfw/ti-fs-stub-firmware-am62x-gp-signed.bin",
     sub_dir: "ti-sysfw",